6f41bcfda69c2fc26564764c5c595241b065c3a0
Blake Ba <blake.elink@gmail.com>
2020-06-19 08:45:04 -0400

    added CmakeFiles directory to repo

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/code/third_game/src/builder.cpp" "C:/code/third_game/build/CMakeFiles/game.dir/builder.cpp.obj"
+  "C:/code/third_game/src/entity.cpp" "C:/code/third_game/build/CMakeFiles/game.dir/entity.cpp.obj"
+  "C:/code/third_game/src/global.cpp" "C:/code/third_game/build/CMakeFiles/game.dir/global.cpp.obj"
+  "C:/code/third_game/src/main1.cpp" "C:/code/third_game/build/CMakeFiles/game.dir/main1.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/builder.cpp.obj"
+  "CMakeFiles/game.dir/entity.cpp.obj"
+  "CMakeFiles/game.dir/global.cpp.obj"
+  "CMakeFiles/game.dir/main1.cpp.obj"
+  "game.exe"
+  "game.exe.manifest"
+  "game.lib"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
