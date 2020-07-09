15eaaada33bc8417844d74ffaea765fd12f6e99c
Blake Ba <blake.elink@gmail.com>
2020-07-09 17:09:14 -0400

    added msvc solution to profile code

---
+++ src/AI.cpp
@@ -0,0 +1,9 @@
+
+
+// state machine 
+//
+enum state {
+  FLEE, // move away from bullets and enemies
+  FIGHT // follow path and shoot
+};
+
--- src/builder.h
+++ src/builder.h
@@ -9,7 +9,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
-
+#include <optional>
 #include "entity.h"
 #include "stdlibs.h"
 
